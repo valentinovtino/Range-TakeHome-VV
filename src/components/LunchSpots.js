753fbb0a04c433ff70d09c9f60d06b21edd16a7b
Valentino Valentin <mothaflora@Valentinos-MacBook-Pro.local>
2022-01-17 18:57:51 -0700

    import useForm Hook, create form and handle submit functionality

--- src/components/LunchSpots.js
+++ src/components/LunchSpots.js
@@ -1,10 +1,13 @@
 import React, {useState} from 'react';
 import '../stylesheets/LunchSpots.css';
 import {mockObj} from '../api/helper';
+import {useForm} from 'react-hook-form';
 
 const LunchSpots = () => {
     const [lunches, setLunches] = useState(mockObj);
     const [lunchGenerator, setLunchGenerator] = useState({})
+    const { register, formState, handleSubmit} = useForm();
+
 
     const randomLunch = () => {
         const length = lunches.length
@@ -12,11 +15,40 @@ const LunchSpots = () => {
         setLunchGenerator(lunches[randomVal]);   
     }
 
+    const onSubmit = (data) => {
+        console.log(data)
+        setLunches([...lunches, data])
+    }
+
     return (
         <div>
             <h1>Lunch App</h1>
             <button onClick={()=>randomLunch()}>Generate Lunch</button>
             <h2 className='lunchName'>{lunchGenerator.name}</h2>
+
+            <form onSubmit={handleSubmit(onSubmit)}>
+                    <input
+                        type='text' 
+                        placeholder='Name' 
+                        name='name'
+                        />
+                    <input
+                        type='text' 
+                        placeholder='Address'
+                        name="address" 
+                        />     
+                    <input
+                        type='url' 
+                        placeholder='https://example.com'
+                        name="link" 
+                        />
+                    <input
+                        type='text' 
+                        placeholder='Price Point'
+                        name="price" 
+                        />  
+                    <input className='formSubmit' type='submit'/>
+                </form>
 
         </div>
     )   
