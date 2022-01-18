aae8c3940ffa46631e217cebec446bbbe4bbed0d
Valentino Valentin <mothaflora@Valentinos-MacBook-Pro.local>
2022-01-17 18:32:58 -0700

    successful render of mock database

--- src/components/LunchSpots.js
+++ src/components/LunchSpots.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../stylesheets/LunchSpots.css';
+import {mockObj} from '../api/helper';
 
 const LunchSpots = () => {
+    const [lunches, setLunches] = useState(mockObj);
+
     return (
         <div>
-            <h1>Lunch Spot!</h1>
+            <h1>{lunches.map(x=>x.name)}</h1>
         </div>
     )   
 };
