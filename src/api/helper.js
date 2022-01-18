986258c73ad9edce02ff3a5477d6439ea9599de8
Valentino Valentin <mothaflora@Valentinos-MacBook-Pro.local>
2022-01-17 18:29:51 -0700

    created styledsheet and mockObj for mock database

---
+++ src/api/helper.js
@@ -0,0 +1,50 @@
+export const mockObj = [
+    {
+        name: 'City O\' City',
+        address: '206 E 13th Ave, Denver, CO 80203',
+        link: 'http://cityocitydenver.com/',
+        price: '$$',
+    },
+    {
+        name: 'Watercourse Foods',
+        address: '837 E 17th Ave, Denver, CO 80218',
+        link: 'https://www.watercoursefoods.com/',
+        price: '$$',
+    },
+    {
+        name: 'Vital Root',
+        address: '3915 Tennyson St, Denver, CO 80212',
+        link: 'https://www.ediblebeats.com/vital-root',
+        price: '$$',
+    },
+    {
+        name: 'El Five',
+        address: '2930 15th St Unit #500, Denver, CO 80211',
+        link: 'https://www.elfivedenver.com/',
+        price: '$$$',
+    },
+    {
+        name: 'Root Down',
+        address: '1600 W 33rd Ave, Denver, CO 80211',
+        link: 'https://www.ediblebeats.com/root-down',
+        price: '$$$',
+    },
+    {
+        name: 'Linger',
+        address: '2030 W 30th Ave, Denver, CO 80211',
+        link: 'https://www.ediblebeats.com/linger',
+        price: '$$$',
+    },
+    {
+        name: 'Happy Camper',
+        address: '3211 N Pecos St, Denver, CO 80211',
+        link: 'http://happycamper.pizza/denver',
+        price: '$',
+    },
+    {
+        name: 'Recess Beer Garden',
+        address: '2715 17th St #103, Denver, CO 80211',
+        link: 'https://www.recessbeergarden.com/',
+        price: '$$',
+    },
+];
--- src/components/LunchSpots.js
+++ src/components/LunchSpots.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../stylesheets/LunchSpots.css';
 
 const LunchSpots = () => {
     return (
